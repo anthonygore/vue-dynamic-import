9f6c7f4720317c1dd7459b7626c38d6109510696
Anthony Gore <anthony@magnum.technology>
2017-10-20 22:46:00 +1100

    Removing computed property

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
+import BooksPage from './pages/BooksPage.js';
+
 new Vue({
   el: '#app',
   data: {
-    route: '/pages/BooksPage.js'
+    page: BooksPage
   },
   methods: {
     navigate(event) {
-      this.route = event.target.pathname;
-    }
-  },
-  computed: {
-    page() {
-      //console.log(this.route); // Uncomment this and it works
-      return () => import(
+      this.page = () => import(
         /* webpackChunkName: "pages/[request]" */
-        `./pages/${this.route.split('/').pop()}`
+        `./pages/${event.target.pathname.split('/').pop()}`
       );
     }
   }
